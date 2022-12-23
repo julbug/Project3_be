6184eed5247801559c442287bc639001b407ce63
Julia Dannerth <jdannerth@gmail.com>
2022-12-22 21:19:00 -0500

    hopefully this works

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,119 +1,83 @@
 const express = require('express');
 const router = express.Router();
-const User = require("../models/User.model")
+const User = require('../models/User.model');
 const bcryptjs = require('bcryptjs');
 
-//================SIGN-UP
 
-router.post('/signup', (req, res, next)=>{
+
+router.post('/signup', (req,res,next) => {
+    console.log({body: req.body})
     const saltRounds = 12;
+    
     bcryptjs
     .genSalt(saltRounds)
     .then(salt => bcryptjs.hash(req.body.password, salt))
     .then(hashedPassword => {
-      // console.log(`Password hash: ${hashedPassword}`);
-      User.create({
+        console.log({hashedPassword});
+    User.create({
         username: req.body.username,
-        email: req.body.email,
         password: hashedPassword,
+        email: req.body.email,
     })
-    res.json({success: true, res});
+    .then(() => {
+        res.json({message: "successfully signed up new account"})
     })
-    .catch(error => next(error));
-});
-
-
-function serializeTheUserObject(userObj){
-  console.log(userObj)
-  let result = {};
-  if(userObj.username) result.username = userObj.username;
-  if(userObj.email) result.email = userObj.email;
-  console.log(result)
-  return result;
-}
-
-
-router.get('/serializeuser', (req, res, next)=>{
-  if(!req.session.currentlyLoggedIn) res.json(null);
-  console.log({session: req.session})
-  User.findById(req.session.currentlyLoggedIn._id)
-  .then((theUser)=>{
-
-    // console.log({theUser, session: req.session})
-
-    res.json(serializeTheUserObject(theUser))
-  })
-  .catch((err)=>{
-    (err)
-  })
+    .catch((err) => {
+        res.json({err, success: false});
+    });
+    });
 })
 
-//=======================LOGIN
-router.post('/login', (req, res, next) => {
-  if (req.body.username === '' || req.body.email === '' || req.body.password === '') {
-    // req.flash('error', 'Please make sure to fill in all fields');
-    res.json({success: false, message: 'Please make sure to fill in all fields'});
-    return;
-  }
- 
-  User.findOne({ username: req.body.username })
+router.post('/login', (req,res,next) => {
+    if (req.body.username === '' || req.body.password === '') {
+        res.json({error: "fields cannot be left blank"})
+        return;
+    }
+
+    User.findOne({ username: req.body.username })
     .then(resultFromDB => {
-      if (!resultFromDB) {
-        res.json('error')
-        return;
-      } else if (bcryptjs.compareSync(req.body.password, resultFromDB.password)) {
-        req.session.currentlyLoggedIn = resultFromDB;
-        req.session.save();
+        if (!resultFromDB) {
+        res.json({error: "username/password combination is not correct"});
+            return;
+        } else if (bcryptjs.compareSync(req.body.password, resultFromDB.password)) {
+            req.session.currentlyLoggedIn = resultFromDB;
+            res.json({message: "Successfully logged in"});
+            return;
+        } else {
+            res.json({error: "username/password combination is not correct"});
+        }
+        })
+        .catch(error => console.log(error));
+})
 
-        console.log(req.session);
-
-        res.json({success: true, result: resultFromDB});
-        return;
-      } else {
-        res.json('error');
-      }
+function serializeTheUserObject(userObj){
+    let result = {};
+    if(userObj.username) result.username = userObj.username;
+    if(userObj.email) result.email = userObj.email;
+    return result;
+  }
+  
+  
+  router.get('/serializeuser', (req, res, next)=>{
+    console.log(req.session);
+    console.log(req.session.currentlyLoggedIn);
+  
+    if(!req.session.currentlyLoggedIn) res.json(null);
+  
+    User.findById(req.session.currentlyLoggedIn._id)
+    .then((theUser)=>{
+      res.json(serializeTheUserObject(theUser))
     })
-    .catch(error => console.log(error));
-});
+    .catch((err)=>{
+      console.log(err)
+    })
+  })
 
-//================ LOGOUT
-
-router.post("/logout", (req, res, next) => {
-  req.session.destroy((err) => {
-    if (err) console.log(err);
-    res.json({ message: "successfully logged out" });
-  });
-});
-
-
-//================ CHANGE PASSWORD
-
-// router.post('/new-password', (req, res, next)=>{
-
-//   if(req.body.newpass !== req.body.confirmnewpass){
-//   }
-
-//   User.findById(req.session.currentlyLoggedIn._id)
-//   .then(resultFromDB => {
-//      if (bcryptjs.compareSync(req.body.oldpass, resultFromDB.password)) {
-//       const saltRounds = 12;
-//       bcryptjs
-//       .genSalt(saltRounds)
-//       .then(salt => bcryptjs.hash(req.body.newpass, salt))
-//       .then(hashedPassword => {
-        
-//         User.findByIdAndUpdate(req.session.currentlyLoggedIn._id, {
-//           password: hashedPassword
-//         })
-//         .then(()=>{
-
-//         })
-//       })
-//         .catch((err)=>{
-//           next(err);
-//         })
-//   }
-// })
-// })
+router.post('/logout', (req, res, next)=>{
+    req.session.destroy(err => {
+        if (err) console.log(err);
+        res.json({message: "successfully logged out"});
+    });
+})
 
 module.exports = router;
---
+++ routes/auth.routesJULIA.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const router = express.Router();
+const User = require("../models/User.model")
+const bcryptjs = require('bcryptjs');
+
+//================SIGN-UP
+
+router.post('/signup', (req, res, next)=>{
+    const saltRounds = 12;
+    bcryptjs
+    .genSalt(saltRounds)
+    .then(salt => bcryptjs.hash(req.body.password, salt))
+    .then(hashedPassword => {
+      // console.log(`Password hash: ${hashedPassword}`);
+      User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+    })
+    res.json({success: true, res});
+    })
+    .catch(error => next(error));
+});
+
+
+function serializeTheUserObject(userObj){
+  console.log(userObj)
+  let result = {};
+  if(userObj.username) result.username = userObj.username;
+  if(userObj.email) result.email = userObj.email;
+  console.log(result)
+  return result;
+}
+
+
+router.get('/serializeuser', (req, res, next)=>{
+  if(!req.session.currentlyLoggedIn) res.json(null);
+  console.log({session: req.session})
+  User.findById(req.session.currentlyLoggedIn._id)
+  .then((theUser)=>{
+
+    // console.log({theUser, session: req.session})
+
+    res.json(serializeTheUserObject(theUser))
+  })
+  .catch((err)=>{
+    (err)
+  })
+})
+
+//=======================LOGIN
+router.post('/login', (req, res, next) => {
+  if (req.body.username === '' || req.body.email === '' || req.body.password === '') {
+    // req.flash('error', 'Please make sure to fill in all fields');
+    res.json({success: false, message: 'Please make sure to fill in all fields'});
+    return;
+  }
+ 
+  User.findOne({ username: req.body.username })
+    .then(resultFromDB => {
+      if (!resultFromDB) {
+        res.json('error')
+        return;
+      } else if (bcryptjs.compareSync(req.body.password, resultFromDB.password)) {
+        req.session.currentlyLoggedIn = resultFromDB;
+        req.session.save();
+
+        console.log(req.session);
+
+        res.json({success: true, result: resultFromDB});
+        return;
+      } else {
+        res.json('error');
+      }
+    })
+    .catch(error => console.log(error));
+});
+
+//================ LOGOUT
+
+router.post("/logout", (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) console.log(err);
+    res.json({ message: "successfully logged out" });
+  });
+});
+
+
+//================ CHANGE PASSWORD
+
+// router.post('/new-password', (req, res, next)=>{
+
+//   if(req.body.newpass !== req.body.confirmnewpass){
+//   }
+
+//   User.findById(req.session.currentlyLoggedIn._id)
+//   .then(resultFromDB => {
+//      if (bcryptjs.compareSync(req.body.oldpass, resultFromDB.password)) {
+//       const saltRounds = 12;
+//       bcryptjs
+//       .genSalt(saltRounds)
+//       .then(salt => bcryptjs.hash(req.body.newpass, salt))
+//       .then(hashedPassword => {
+        
+//         User.findByIdAndUpdate(req.session.currentlyLoggedIn._id, {
+//           password: hashedPassword
+//         })
+//         .then(()=>{
+
+//         })
+//       })
+//         .catch((err)=>{
+//           next(err);
+//         })
+//   }
+// })
+// })
+
+module.exports = router;
